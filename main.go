4a09a3b1fd197263ded619c4c98b2352ecf79def
RRLouw <rrlrrlouw@gmail.com>
2012-07-04 17:04:09 +0200

    Removed global variables

--- main.go
+++ main.go
@@ -12,32 +12,21 @@ var (
 	length      int
 	help        bool
 	geo         bool
-	counter     int = 0
 	writestream     = make(chan []byte)
 )
 
 func init() {
-	flag.IntVar(&length, "l", 10, "number of tweets to dump")
-	flag.BoolVar(&help, "h", false, "helpfile")
+	flag.IntVar(&length, "l", 10, "number of tweets to dump: default = 10: for uninterupted stream l=0")
 	flag.BoolVar(&geo, "g", false, "only dump tweets with coordinate values")
 }
+
 func main() {
 	flag.Parse()
-	if help {
-		helpfile()
-	} else if flag.Arg(0) != "" && flag.Arg(1) != "" {
+	if flag.Arg(0) != "" && flag.Arg(1) != "" {
 		execute()
 	} else {
 		fmt.Println("Invalid Use. Use -h for help")
 	}
-}
-
-func helpfile() {
-	fmt.Println("\n -h   : helpfile")
-	fmt.Println(" -g   : only dump tweets with coordinate values")
-	fmt.Println(" -l=i : A total of i tweets will be dumped (default = 10)")
-	fmt.Println("You need to enter a valid twitter account's username and password")
-	fmt.Println("example: main.go -g -l=100 Username PaSsWoRd\n")
 }
 
 func execute() {
--- streamer/streamer.go
+++ streamer/streamer.go
@@ -3,36 +3,41 @@ package streamer
 import (
 	"encoding/json"
 	"tweetdumper/twitterstream"
-)
-
-var (
-	counter = 0
-	stream  = make(chan *twitterstream.Tweet)
+	"math"
 )
 
 func Stream(username string, password string, length int, geo bool, ch chan []byte) {
-	connect(username, password)
-	read(length, geo, ch)
+	stream  := make(chan *twitterstream.Tweet)
+	stream, _ = connect(username, password, stream)
+	read(length, geo, stream, ch)
 }
 
-func connect(username string, password string) error {
+func connect(username string, password string, stream chan *twitterstream.Tweet) (chan *twitterstream.Tweet , error) {
 	client := twitterstream.NewClient(username, password)
-	return client.Sample(stream)
+	err := client.Sample(stream)
+	if err != nil {
+		return nil, err
+	}
+	return stream, err
 }
 
-func read(length int, geo bool, ch chan []byte) error {
+func read(length int, geo bool, stream chan *twitterstream.Tweet, ch chan []byte) (err error) {
 	defer close(ch)
+	counter := 0
+	if length == 0 {
+		length = math.MaxInt32
+	}
 	for counter < length {
 		tw := <-stream
 		if geo {
 			if tw.Coordinates.Coordinates != nil {
-				err := jsonDump(tw, ch)
+				counter, err = jsonDump(tw, counter, ch)
 				if err != nil {
 					return err
 				}
 			}
 		} else {
-			err := jsonDump(tw, ch)
+			counter, err = jsonDump(tw, counter, ch)
 			if err != nil {
 				return err
 			}
@@ -41,11 +46,11 @@ func read(length int, geo bool, ch chan []byte) error {
 	return nil
 }
 
-func jsonDump(tw *twitterstream.Tweet, ch chan []byte) error {
+func jsonDump(tw *twitterstream.Tweet, counter int, ch chan []byte) (int, error) {
 	btw, err := json.Marshal(tw)
 	if err == nil {
 		ch <- btw
 		counter += 1
 	}
-	return err
+	return counter, err
 }
